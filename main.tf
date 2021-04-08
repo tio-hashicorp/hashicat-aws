40bf93958e0b70b81909683939a0552061031dd7
tiobagio <tio@hashicorp.com>
2021-04-08 20:32:44 +0800

    add timeout

--- main.tf
+++ main.tf
@@ -177,6 +177,7 @@ resource "null_resource" "configure-cat-app" {
       user        = "ubuntu"
       private_key = tls_private_key.hashicat.private_key_pem
       host        = aws_eip.hashicat.public_ip
+      timeout = 10m
     }
   }
 }
