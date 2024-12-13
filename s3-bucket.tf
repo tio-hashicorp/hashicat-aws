3ecf3323d5b01f9ca80c566ba6af53dcb3adcebc
Tio Bagio <10209203+tiobagio@users.noreply.github.com>
2024-12-13 16:55:44 +0800

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
   bucket = "tio-s3-bucket"
-  acl    = "private"
+  # acl    = "private"
 
   versioning = {
     enabled = true
