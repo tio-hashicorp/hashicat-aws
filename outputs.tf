30829169a7bbfe043c6536325874d4a674290616
Tio Bagio <10209203+tiobagio@users.noreply.github.com>
2025-03-06 16:53:05 +0800

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,12 @@
 # Outputs file
-output "catapp_url" {
-  value = "http://${aws_eip.hashicat.public_dns}"
-}
 
 output "catapp_ip" {
   value = "http://${aws_eip.hashicat.public_ip}"
 }
 
-output "catapp_ip2" {
-  value = "http://${aws_eip.hashicat.public_ip}"
+output "private_key" {
+  value = tls_private_key.hashicat.private_key_pem
 }
-
+output "catapp_url" {
+  value = "http://${aws_eip.hashicat.public_dns}"
+}
