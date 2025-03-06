77df5644bc73bf3112026fcdbd14040d9655a446
Tio Bagio <10209203+tiobagio@users.noreply.github.com>
2025-03-06 16:54:55 +0800

    Update outputs.tf
    add sensitive=true for tls_private_key

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,7 @@ output "catapp_ip" {
 
 output "private_key" {
   value = tls_private_key.hashicat.private_key_pem
+  sensitive = true
 }
 output "catapp_url" {
   value = "http://${aws_eip.hashicat.public_dns}"
